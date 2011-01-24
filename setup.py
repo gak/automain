45b1c973bcc05b63c2efaa575162f630cf71bb52
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-01-24 20:14:12 +1100

    proper name of module in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 
 from distutils.core import setup
 
-setup(name='Distutils',
+setup(
+    name='automain',
     version='1.0',
     description='A shortcut for "if __name__ == \"main\":"',
     author='Gerald Kaszuba',

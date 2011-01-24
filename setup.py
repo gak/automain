e1f5fd4c979026b9c7d389c937b5c05fadfa0584
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-01-24 18:18:35 +1100

    created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+    version='1.0',
+    description='A shortcut for "if __name__ == \"main\":"',
+    author='Gerald Kaszuba',
+    author_email='automain@gakman.com',
+    packages=['automain'],
+    )
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from automain import *
 
 @automain

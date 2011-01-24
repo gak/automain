176ee4314698265d5b345cd9620424b2a40e4dc4
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-01-25 07:51:37 +1100

    doesnt work as intented when imported from another module

--- automain/__init__.py
+++ automain/__init__.py
@@ -1,4 +1,8 @@
 def automain(func):
-    func()
+    import inspect
+    parent = inspect.stack()[1][0]
+    name = parent.f_locals.get('__name__', None)
+    if name == '__main__':
+        func()
 
 

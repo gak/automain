a106196ab41001c6b75b87ae0e3ac7c3fe058a03
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-01-27 08:33:50 +1100

    updated the readme example slightly. setup.py to point to the blog post

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description='A more aesthetically pleasing replacement ' + \
         'of "if __name__ == \'main\':"',
     long_description=readme,
+    url='http://slowchop.com/2011/01/25/automain/',
     author='Gerald Kaszuba',
     author_email='automain@gakman.com',
     packages=['automain'],
